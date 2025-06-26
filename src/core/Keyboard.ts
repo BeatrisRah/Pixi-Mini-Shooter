1ce10e90e0e44ff11513f6b808ed7ff3a914d409
Beatris <beatrisraj7@gmail.com>
2025-06-26 22:15:36 +0300

    feat: Keyboard class added

---
+++ src/core/Keyboard.ts
@@ -0,0 +1,32 @@
+export class Keyboard {
+    private keys: Record<string, boolean> = {};
+    constructor() {
+        window.addEventListener("keydown", this.onKeyDown);
+        window.addEventListener("keyup", this.onKeyUp);
+    }
+
+    private onKeyDown = (e: KeyboardEvent): void => {
+        this.keys[e.key.toLowerCase()] = true;
+    };
+
+    private onKeyUp = (e: KeyboardEvent): void => {
+        this.keys[e.key.toLowerCase()] = false;
+    };
+
+    public isDown(key: string): boolean {
+        return this.keys[key.toLowerCase()] ?? false;
+    }
+
+    public isUp(key: string): boolean {
+        return !this.isDown(key);
+    }
+
+    public anyDown(...keys: string[]): boolean {
+        return keys.some((k) => this.isDown(k));
+    }
+
+    public destroy(): void {
+        window.removeEventListener("keydown", this.onKeyDown);
+        window.removeEventListener("keyup", this.onKeyUp);
+    }
+}
